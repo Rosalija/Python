2753c8222a197527fc0071e40040a79289095206
Rosalija Pelczmann <99193534+Rosalija@users.noreply.github.com>
2022-03-08 20:47:12 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# Prvi python komentar
+
+
+"""mood = 'happy'
+
+if mood == 'happy':
+    print('It is great to see you happy!')
+    print('hahaha')
+elif mood == 'nervous':
+    print('Take a deep breath 3 times.')
+elif mood == 'anxious':
+    print("Don't worry, be happy!")
+else:
+    print('Cheer up, mate')"""
+
+ime = input('upiši svoje ime: ')
+mood = input('Kako se osječaš? ')
+print("Hej, "+ime+" nemoj biti "+mood)
